e3672623a37fcc7c5f6ed54349c544ede94d64be
Nizam <ramazanov570633@gmail.com>
2023-12-06 15:35:52 +0400

    Don't work

--- App.tsx
+++ App.tsx
@@ -1,50 +1,61 @@
-import React, { useRef } from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 
-const App = () => {
-  const tokyoRegion = {
-    latitude: 35.6762,
-    longitude: 139.6503,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
-  };
-
-  const mapRef = useRef(null);
-  console.log(mapRef);
-
-
+export default function App() {
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={tokyoRegion}
-        ref={mapRef}
+        initialRegion={{
+          latitude: 40.4093,
+          longitude: 49.8671,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
       >
-        {/* <Marker
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          title="Marker Title"
-          description="Marker Description"
-        /> */}
+        <Marker
+          coordinate={{ latitude: 40.4093, longitude: 49.8671 }}
+          title="Baku"
+          description="The capital of Azerbaijan"
+        >
+          <Callout>
+            <View style={styles.callout}>
+              <Text style={styles.title}>Baku</Text>
+              <Text style={styles.description}>
+                Baku is the largest city and the cultural, industrial and
+                political center of Azerbaijan. It is located on the southern
+                shore of the Absheron Peninsula, which projects into the Caspian
+                Sea.
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
       </MapView>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   map: {
+    width: '100%',
+    height: '100%',
     ...StyleSheet.absoluteFillObject,
   },
-  text: {
-    fontSize: 20,
-    backgroundColor: "lightblue",
+  callout: {
+    width: 200,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  description: {
+    fontSize: 14,
   },
 });
-
-export default App;
